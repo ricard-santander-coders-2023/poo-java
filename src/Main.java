8767e40e56d75f6963e03cd0ae75f5e7b8cee21e
Ricard Roberg <ricardroberg@gmail.com>
2023-11-14 09:06:34 -0300

    unico commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+//  public static void main(String... args) {
+
+        System.out.println("Hello World!");
+
+        byte variavelByte = -127;
+        //short = -32768 a 32767
+        //char 0 a 65535
+        //int -2b a 2b
+        //long -9t a 9t
+
+        int variavelInt = 2147483;
+
+        System.out.println(variavelInt);
+
+
+        System.out.println(variavelByte);
+
+    }
+}
