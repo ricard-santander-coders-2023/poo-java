2ad67b29014cd414bf7ad2ad9d95c0ba4d209f6a
Ricard Roberg <ricardroberg@gmail.com>
2023-11-15 14:02:50 -0300

    comentarios, padrao variaveis, cast e outros

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,41 @@ public class Main {
     public static void main(String[] args) {
 
         Cachorro meuDog = new Cachorro("Fuffy", "mista", "branco", 42, 32.5);
-
         Gato bichano = new Gato("Garfield", "persa", "laranja/listras pretas", 28, 45);
-
         Passaro piuPiu = new Passaro("Piu-piu", "canario", "amarelo", 0.5);
 
-       meuDog.soar();
-       bichano.soar();
-       piuPiu.soar();
+//comentário de uma linha
+        meuDog.soar();
+
+        bichano.soar();
+        piuPiu.soar();
+        /*
+            comentário de varias linhas
+            comentário de varias linhas
+            comentário de varias linhas
+         */
+
+        // declaracao de variaveis
+        //        int _$teste; //nao ideal
+        //        double ______;  //nao ideal
+        //    String int = "numero";  //nao ideal
+        int inteiro;
+        long longo;
+        double ______ = 23.5;
+        float $$;
+        boolean $87;
+        char ch;
+        String teste;
+        // valor padrao para variaveis não iniciadas é null
+        // Ideal é iniciar com letra minuscula, _  ou $ e seguir o padrão camel-case
+
+        double d = 5d;
+        float f = 3.00f;
+
+        float x = f + (float) d;
+        System.out.println(x);
+
+        int a = 234_234_234; // usando _ para separar
+
     }
 }
